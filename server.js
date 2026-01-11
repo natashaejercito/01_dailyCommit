d8e60fcc1eadf3434860190fd2ebded21970bf6f
Natasha Nicole Ejercito <natasha.ejercito@outlook.com>
2026-01-10 23:22:44 -0500

    testing paths

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const booksRead = require("./books.json");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send({ message: `Updating this file in progress` });
+});
+
+app.get("/books", (req, res) => {
+  res.send({ booksRead });
 });
 
 app.listen(HTTP_PORT, () => {
